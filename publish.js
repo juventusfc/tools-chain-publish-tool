96b6d9b804170f636ae2a29f86686d854e1b6a26
FrankHu <frank.hu@perficient.com>
2020-08-18 14:40:23 +0800

    ok

--- publish.js
+++ publish.js
@@ -1,16 +1,38 @@
 const http = require("http");
+const querystring = require("querystring");
 
+const postData = querystring.stringify({
+  content: "Hello World!",
+});
 const options = {
   host: "127.0.0.1",
   port: 8081,
   path: "/?filename=x.html",
-  method: "GET",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Content-Length": Buffer.byteLength(postData),
+  },
 };
 
-// Make a request
-const req = http.request(options);
-req.end();
-
-req.on("information", (info) => {
-  console.log(`Got information prior to main response: ${info.statusCode}`);
+const req = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  /*
+  res.setEncoding("utf8");
+  res.on("data", (chunk) => {
+    console.log(`BODY: ${chunk}`);
+  });
+  res.on("end", () => {
+    console.log("No more data in response.");
+  });
+  */
 });
+
+req.on("error", (e) => {
+  console.error(`problem with request: ${e.message}`);
+});
+
+// Write data to request body
+req.write(postData);
+req.end();
