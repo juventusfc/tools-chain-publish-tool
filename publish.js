8486933b35a86819a2912852eb18e1254c65e68f
FrankHu <frank.hu@perficient.com>
2020-08-19 12:59:58 +0800

    re

--- publish.js
+++ publish.js
@@ -19,15 +19,6 @@ fs.stat(filename, (error, stat) => {
   const req = http.request(options, (res) => {
     console.log(`STATUS: ${res.statusCode}`);
     console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    /*
-    res.setEncoding("utf8");
-    res.on("data", (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    });
-    res.on("end", () => {
-      console.log("No more data in response.");
-    });
-    */
   });
 
   req.on("error", (e) => {
@@ -36,7 +27,6 @@ fs.stat(filename, (error, stat) => {
 
   const readStream = fs.createReadStream("./m8.jpg");
 
-  // Write data to request body
   readStream.pipe(req);
   readStream.on("end", () => {
     req.end();
