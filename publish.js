2d2b6c6c3a6697dfa1b7872ae2ceeeb65fcb948b
FrankHu <frank.hu@perficient.com>
2020-08-19 13:43:29 +0800

    add

--- publish.js
+++ publish.js
@@ -1,34 +1,42 @@
 const http = require("http");
 const fs = require("fs");
-const querystring = require("querystring");
+var archiver = require("archiver");
 
-let filename = "./m8.jpg";
-fs.stat(filename, (error, stat) => {
-  console.log(stat.size);
-  const options = {
-    host: "127.0.0.1",
-    port: 8081,
-    path: "/?filename=" + filename,
-    method: "POST",
-    headers: {
-      "Content-Type": "application/octet-stream",
-      "Content-Length": stat.size,
-    },
-  };
-
-  const req = http.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  });
-
-  req.on("error", (e) => {
-    console.error(`problem with request: ${e.message}`);
-  });
-
-  const readStream = fs.createReadStream(filename);
-
-  readStream.pipe(req);
-  readStream.on("end", () => {
-    req.end();
-  });
+let packname = "./package";
+var archive = archiver("zip", {
+  zlib: { level: 9 }, // Sets the compression level.
 });
+
+archive.directory(packname, false);
+
+const options = {
+  host: "127.0.0.1",
+  port: 8081,
+  path: "/?filename=package.zip",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/octet-stream",
+  },
+};
+
+const req = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+});
+
+req.on("error", (e) => {
+  console.error(`problem with request: ${e.message}`);
+});
+
+// const readStream = fs.createReadStream(packname);
+
+// readStream.pipe(req);
+// readStream.on("end", () => {
+//   req.end();
+// });
+
+archive.pipe(req);
+archive.on("end", () => {
+  req.end();
+});
+archive.finalize();
